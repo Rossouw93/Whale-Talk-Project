46dab7332b7b521856d6c9639fc2c943804308eb
Rossouw93 <139475020+Rossouw93@users.noreply.github.com>
2023-11-14 22:27:38 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+var input = 'turpentine and turtles';
+var input = input.toLowerCase();
+
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+var resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    // console.log(input[i], i);
+    const inputIterate = input[i];
+    if (input[i] == 'e') {
+        resultArray.push(input[i])
+    };
+    if (input[i] == 'u') {
+        resultArray.push(input[i]) 
+    };
+        
+    for (let j = 0; j < vowels.length; j++) {
+        // console.log(vowels[j]);
+        const vowelsIterate = vowels[j];
+        if(inputIterate === vowelsIterate){
+            resultArray.push(vowelsIterate);
+        }
+    } 
+    
+}
+var resultString = resultArray.join(''); 
+var resultString = resultString.toUpperCase();
+
+console.log(resultString)
